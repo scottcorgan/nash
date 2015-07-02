2ba3a43e8942b18f07b9148a63f6c8cc27575238
Scott Corgan <scottcorgan@gmail.com>
2015-07-02 13:18:19 -0700

    eslint all the things

--- test/flag.js
+++ test/flag.js
@@ -3,10 +3,10 @@ var test = require('tape');
 var flag = require('../lib/flag');
 
 test('flag: extends EventEmitter', function (t) {
-  
+
   var EventEmitter = require('events').EventEmitter;
   var flg = flag('-t');
-  
+
   t.ok(flg.emit, 'emit');
   t.ok(flg.addListener, 'addListener');
   t.ok(flg.on, 'on');
@@ -14,23 +14,23 @@ test('flag: extends EventEmitter', function (t) {
   t.ok(flg.removeListener, 'removeListener');
   t.ok(flg.removeAllListeners, 'removeAllListeners');
   t.ok(flg.listeners, 'listeners');
-  
+
   t.end();
 });
 
 test('flag: handler', function (t) {
-  
+
   var handlerCalled = false;
   var flg = flag('test')
     .handler(function (done) {
-      
+
       handlerCalled = true;
-      
+
       done();
     });
-  
+
   flg.handler()(function () {
-    
+
     t.equal(typeof flg.handler(), 'function', 'gets function');
     t.ok(handlerCalled, 'sets the function');
     t.end();
@@ -38,19 +38,19 @@ test('flag: handler', function (t) {
 });
 
 test('flag: getters and setters', function (t) {
-  
+
   var flg = flag('-f')
     .override(true);
-  
+
   t.deepEqual(flg.name(), ['-f'], 'name');
   t.equal(flg.shouldOverride(), true, 'override');
   t.end();
 });
 
 test('flag: matches flag name', function (t) {
-  
+
   var flg = flag('--test', '-t');
-  
+
   t.ok(flg.matchesName('--test'), 'matches single name');
   t.ok(flg.matchesName('-t'), 'matches other names');
   t.ok(flg.matchesName(['-t']), 'accepts and array of names');
@@ -58,24 +58,24 @@ test('flag: matches flag name', function (t) {
 });
 
 test('flag: running the flag', function (t) {
-  
+
   var flagCalled = false;
   var flagCallCount = 0;
   var flg = flag('-t')
     .handler(function (data, done) {
-      
+
       flagCalled = true;
       flagCallCount += 1;
-      
+
       t.equal(data, 'data', 'flag value');
-      
+
       done();
     });
-  
+
   flg.run('data', function () {
-    
+
     flg.runOnce('data', function () {
-      
+
       t.ok(flagCalled, 'ran flag');
       t.equal(flagCallCount, 1, 'called only once when using runOnce()');
       t.ok(flg.internals.ran, 'flag ran set to true');
@@ -85,7 +85,7 @@ test('flag: running the flag', function (t) {
 });
 
 test('flag: prefixes', function (t) {
-  
+
   t.equal(flag.prefix('test'), '--test', '2 dashes to a word');
   t.equal(flag.prefix('t'), '-t', '1 dash for a letter');
   t.equal(flag.prefix('--test'), '--test', 'add no dashes for a name with dashes already');
@@ -94,7 +94,7 @@ test('flag: prefixes', function (t) {
 });
 
 test('flag: unprefixes', function (t) {
-  
+
   t.equal(flag.unprefix('--test'), 'test', '2 dashes to a word');
   t.equal(flag.unprefix('-t'), 't', '1 dash for a letter');
   t.equal(flag.unprefix('test'), 'test', 'add no dashes for a name with no dashes already');
@@ -103,11 +103,11 @@ test('flag: unprefixes', function (t) {
 });
 
 test.skip('flag: default value', function (t) {
-  
+
   var f = flag();
-  
+
   f.default('my value');
-  
+
   t.equal(f.default(), 'my value', 'set default');
   t.deepEqual(f.default('another value'), f, 'chainable');
   t.end();
--- test/flags.js
+++ test/flags.js
@@ -4,16 +4,16 @@ var flags = require('../lib/flags');
 var flag = require('../lib/flag');
 
 test('flags: instance', function (t) {
-  
+
   t.ok(flags() instanceof flags.Instance, 'instance of Flags');
   t.end();
 });
 
 test('flags: instantiates with a list of flags', function (t) {
-  
+
   var flg = flag('-t');
   var flg2 = flag('-t2');
-  
+
   t.deepEqual(flags(flg).all(), [flg], 'with single flag');
   t.deepEqual(flags([flg]).all(), [flg], 'with single flag in array');
   t.deepEqual(flags([flg, flg2]).all(), [flg, flg2], 'with multiple flags in array');
@@ -22,19 +22,19 @@ test('flags: instantiates with a list of flags', function (t) {
 });
 
 test('flags: raw array of flags', function (t) {
-  
+
   var flg = flag('-t');
   var flgs = flags(flg);
-  
+
   t.deepEqual(flgs.all(), [flg], 'list of flags');
   t.end();
 });
 
 test('flags: add flag to collection', function (t) {
-  
+
   var flg = flag('-t');
   var flg2 = flag('-t2');
-  
+
   t.deepEqual(flags().add(flg).all(), [flg], 'added single flag');
   t.deepEqual(flags().add([flg]).all(), [flg], 'added single flag in array');
   t.deepEqual(flags().add([flg, flg2]).all(), [flg, flg2], 'added multiple flags in array');
@@ -43,18 +43,18 @@ test('flags: add flag to collection', function (t) {
 });
 
 test('flags: does not overwrite flag when adding to collection', function (t) {
-  
+
   var flgs = flags();
   var handler = function () {/* handler */};
-  
+
   var flg = flag('--test')
     .handler(handler);
-  
+
   var flg2 = flag('--test', '-t');
-  
+
   flgs.add(flg);
   flgs.add(flg2);
-  
+
   t.equal(flgs.all().length, 1, 'one uqique');
   t.equal(flgs.findByName('test').handler().toString(), handler.bind(flg).toString(), 'same flg handler');
   t.ok(flgs.findByName('-t'), 'adds missing names when flags match');
@@ -62,55 +62,55 @@ test('flags: does not overwrite flag when adding to collection', function (t) {
 });
 
 test('flags: find flag by name', function (t) {
-  
+
   var flg1 = flag('-t1', '--test1', '--another1');
   var flg2 = flag('--test2', '-t2');
   var flgs = flags(flg1, flg2);
-  
+
   t.deepEqual(flgs.findByName('--test1'), flg1, 'by single name');
   t.deepEqual(flgs.findByName(['--test1']), flg1, 'by single name in array');
   t.deepEqual(flgs.findByName(['--test1', '-t1']), flg1, 'by multiple names in array');
   t.deepEqual(flgs.findByName('--test1', '-t1'), flg1, 'by multiple names as arguments');
-  
+
   t.deepEqual(flgs.findByName('test1'), flg1, 'by single name with no dashes');
   t.deepEqual(flgs.findByName(['test1', '-t1']), flg1, 'by multiple names with no dashes');
-  
+
   t.end();
 });
 
 test('flags: runs flags', function (t) {
-  
+
   t.plan(5);
-  
+
   var flg1Ran = false;
   var flg2Ran = false;
-  
+
   var flg1 = flag('-t')
     .handler(function (value, done) {
-      
+
       t.equal(value, 't value', 'passed in value');
-      
+
       flg1Ran = true;
       done();
     });
-    
+
   var flg2 = flag('--test2')
     .handler(function (value, done) {
-      
+
       t.equal(value, 'test2 value', 'passed in value');
       t.ok(typeof done === 'function', 'passed in callback');
-      
+
       flg2Ran = true;
       done();
     });
-  
+
   var flgs = flags(flg1, flg2);
-  
+
   flgs.run({
     t: 't value',
     test2: 'test2 value'
   }, function (err) {
-    
+
     t.ok(flg1Ran, 'ran flag 1');
     t.ok(flg2Ran, 'ran flag 2');
   });
